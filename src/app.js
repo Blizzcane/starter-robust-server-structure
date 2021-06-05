06bfa58eecbb3bf6c743aa1ff84adb525589ba4a
Blizzcane <blizzcane@gmail.com>
2021-06-05 16:45:52 -0500

    update not working

--- src/app.js
+++ src/app.js
@@ -1,17 +1,42 @@
 const express = require("express");
 const app = express();
+const flipsRouter = require("./flips/flips.router");
 
-// TODO: Follow instructions in the checkpoint to implement ths API.
+const flips = require("./data/flips-data");
+const counts = require("./data/counts-data");
+
+app.use(express.json());
+
+app.use("/counts/:countId", (request, response, next) => {
+  const { countId } = request.params;
+  const foundCount = counts[countId];
+
+  if (foundCount === undefined) {
+    next({ status: 404, message: `Count id not found: ${countId}` });
+  } else {
+    response.json({ data: foundCount });
+  }
+});
+
+app.use("/counts", (request, response) => {
+  response.json({ data: counts });
+});
+
+ 
+app.use("/flips", flipsRouter);
+
+
 
 // Not found handler
-app.use((request, response, next) => {
-  next(`Not found: ${request.originalUrl}`);
+app.use((req, res, next) => {
+  next(`Not found: ${req.originalUrl}`);
 });
 
 // Error handler
-app.use((error, request, response, next) => {
+app.use((error, req, res, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  res.status(status).json({ error: message });
 });
 
 module.exports = app;
