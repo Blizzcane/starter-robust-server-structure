d52f544826c584878f0ccaa1434ad0178589cb64
Blizzcane <blizzcane@gmail.com>
2021-06-05 16:46:02 -0500

    update needs to be fixed

---
+++ src/flips/flips.controller.js
@@ -0,0 +1,85 @@
+const flips = require("../data/flips-data");
+const counts = require("../data/counts-data");
+
+function flipExists(req, res, next) {
+  const { flipId } = req.params;
+  const foundFlip = flips.find((flip) => flip.id === Number(flipId));
+  if (foundFlip) {
+    return next();
+  }
+  next({
+    status: 404,
+    message: `Flip id not found: ${flipId}`,
+  });
+}
+
+function read(req, res) {
+  const { flipId } = req.params;
+  const foundFlip = flips.find((flip) => flip.id === Number(flipId));
+  res.json({ data: foundFlip });
+}
+
+let lastFlipId = flips.reduce((maxId, flip) => Math.max(maxId, flip.id), 0);
+
+function bodyHasResultProperty(req, res, next) {
+  const { data: { result } = {} } = req.body;
+  if (result) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: "A 'result' property is required",
+  });
+}
+
+function resultPropertyIsValid(req, res, next) {
+  const { data: { result } = {} } = req.body;
+  const validResult = ["heads", "tails", "edge"];
+  if (validResult.includes(result)) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: `Value of the 'result' property must be one of ${validResult}. Received: ${result}`,
+  });
+}
+
+function create(req, res) {
+  const { data: { result } = {} } = req.body;
+  const newFlip = {
+    id: ++lastFlipId,
+    result,
+  };
+  flips.push(newFlip);
+  counts[result] = counts[result] + 1;
+  res.status(201).json({ data: newFlip });
+}
+
+function list(req, res) {
+  res.json({ data: flips });
+}
+
+function update(req, res) {
+  const { flipId } = req.params;
+  const foundFlip = flips.find((flip) => flip.id === Number(flipId));
+
+  const originalResult = foundFlip.result;
+  const { data: { result } = {} } = req.body;
+
+  if (originalResult !== result) {
+    // update the flip
+    foundFlip.result = result;
+    // Adjust the counts
+    counts[originalResult] = counts[originalResult] - 1;
+    counts[result] = counts[result] + 1;
+  }
+
+  res.json({ data: foundFlip });
+}
+
+module.exports = {
+  create: [bodyHasResultProperty, resultPropertyIsValid, create],
+  list,
+  read: [flipExists, read],
+  update: [flipExists, bodyHasResultProperty, resultPropertyIsValid, update],
+};
